740f9ce6a9d2575f0e2185aacda1191e46580721
EmilOgard <emilogard@gmail.com>
2025-10-07 20:50:36 +0200

    Update info pages

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const About = () => {
     return (
         <div>
-            <h1>About!</h1>
+            <h1>About:</h1>
+            <p>28 years. Programmer. Bachelor in Computer Science</p>
         </div>
     )
 }
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,7 +4,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const Contact = () => {
     return (
         <div>
-            <h1>Contact!</h1>
+            <h1>Contact info:</h1>
+            <p>Email: emilogard@gmail.com</p>
         </div>
     )
 }
