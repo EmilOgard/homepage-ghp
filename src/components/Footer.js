05e7938c80aa2761032f6b5ee03fa30268c4fbc5
EmilOgard <emilogard@gmail.com>
2026-01-17 01:43:18 +0100

    Change year in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
     return (
         <footer className=" text-center text-lg-start mt-4">
             <div className="text-center p-3">
-                <div id="footer-text">© 2025 Emil Øgård</div>
+                <div id="footer-text">© 2026 Emil Øgård</div>
             </div>
         </footer>
     );
